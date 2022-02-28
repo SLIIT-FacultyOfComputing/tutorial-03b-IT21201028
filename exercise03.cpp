bb692ec59287e95ace31b52b4b75b26876bef671
IT21201028 <99607931+IT21201028@users.noreply.github.com>
2022-02-28 19:13:40 +0000

    Completed

--- exercise03.cpp
+++ exercise03.cpp
@@ -6,6 +6,12 @@ int volume(int height, int width, int length);
 
 // 1. Define a structure called Box
 // have the integer data types Height, Width, Length
+struct Box
+{
+  int Height;
+  int Length;
+  int Width;
+};
 
 // Do not change the main function
 int main() {
@@ -17,27 +23,33 @@ int main() {
     int totalVolume;
     
     // 4. Input the height, width, lenght of box1 and box2
+    Box Box1;
     cout << "Enter Box 1 Height : ";
-    cin >> Height of Box 1;
+    cin >> Box1.Height;
     cout << "Enter Box 1 Width : ";
-    cin >> Width of Box 1;
+    cin >> Box1.Width;
     cout << "Enter Box 1 Length : ";
-    cin >> Length of Box 1;
-    
+    cin >> Box1.Length;
+  
+    Box Box2;
     cout << "Enter Box 2 Height : ";
-    cin >> Height of Box 2;
+    cin >> Box2.Height;
     cout << "Enter Box 2 Width : ";
-    cin >> Width of Box 2;
+    cin >> Box2.Width;
     cout << "Enter Box 2 Length : ";
-    cin >> Length of Box 2;
+    cin >> Box2.Length;
     
     // 5. Replace the coding below to pass the Box type structure
-    totalVolume = volume( , , )
-             + volume( , , );
+    totalVolume = volume(Box1.Height,Box1.Width, Box1.Length )
+             + volume(Box2.Height ,Box2.Width ,Box2.Length );
     
     cout << "Volume of Box is " << totalVolume << endl;
     
     return 0;
 }
 
-// Implement the functions here
+
+inline int volume(int height,int width,int length)
+{
+  return height*width*length;
+}
